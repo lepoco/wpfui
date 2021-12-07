fb403d2d29cfcb9bd60c58efe72787b12fa15a22
Pomianowski <kontakt@rapiddev.pl>
2021-12-07 17:44:38 +0100

    Add placeholders for Tray and Toast [-ci skip]

---
+++ WPFUI/Tray/Tray.cs
@@ -0,0 +1,12 @@
+﻿// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+namespace WPFUI.Tray
+{
+    internal class Tray
+    {
+        // TODO: Implement native tray without forms
+    }
+}
