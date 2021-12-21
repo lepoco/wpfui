9fe2486b92db2b8a3d9417d3b6d091f4d761538a
Pomianowski <kontakt@rapiddev.pl>
2021-12-21 21:43:13 +0100

    Replace CardCollapse with CardExpander [-ci skip]

---
+++ WPFUI/Controls/CardExpander.cs
@@ -0,0 +1,76 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using System.Windows;
+
+namespace WPFUI.Controls
+{
+    /// <summary>
+    /// Inherited from the <see cref="System.Windows.Controls.Expander"/> control which can hide the collapsible content.
+    /// </summary>
+    public class CardExpander : System.Windows.Controls.Expander
+    {
+        /// <summary>
+        /// Property for <see cref="Subtitle"/>.
+        /// </summary>
+        public static readonly DependencyProperty SubtitleProperty = DependencyProperty.Register(nameof(Subtitle),
+            typeof(string), typeof(CardExpander), new PropertyMetadata(null));
+
+        /// <summary>
+        /// Property for <see cref="Icon"/>.
+        /// </summary>
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register(nameof(Icon),
+            typeof(Common.Icon), typeof(CardExpander), new PropertyMetadata(Common.Icon.Empty));
+
+        /// <summary>
+        /// Property for <see cref="IconFilled"/>.
+        /// </summary>
+        public static readonly DependencyProperty IconFilledProperty = DependencyProperty.Register(nameof(IconFilled),
+            typeof(bool), typeof(CardExpander), new PropertyMetadata(false));
+
+        /// <summary>
+        /// Property for <see cref="HeaderContent"/>.
+        /// </summary>
+        public static readonly DependencyProperty HeaderContentProperty =
+            DependencyProperty.Register(nameof(HeaderContent), typeof(object), typeof(CardExpander),
+                new PropertyMetadata(null));
+
+        /// <summary>
+        /// Gets or sets text displayed under main <see cref="Title"/>.
+        /// </summary>
+        public string Subtitle
+        {
+            get => (string)GetValue(SubtitleProperty);
+            set => SetValue(SubtitleProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets displayed <see cref="Common.Icon"/>.
+        /// </summary>
+        public Common.Icon Icon
+        {
+            get => (Common.Icon)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        /// <summary>
+        /// Defines whether or not we should use the <see cref="Common.IconFilled"/>.
+        /// </summary>
+        public bool IconFilled
+        {
+            get => (bool)GetValue(IconFilledProperty);
+            set => SetValue(IconFilledProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets additional content displayed next to the chevron.
+        /// </summary>
+        public object HeaderContent
+        {
+            get => GetValue(HeaderContentProperty);
+            set => SetValue(HeaderContentProperty, value);
+        }
+    }
+}
