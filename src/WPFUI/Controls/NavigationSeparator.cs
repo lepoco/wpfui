2937d44b61c88dde274321c4e4eaf6467009d5dd
pomianowski <kontakt@rapiddev.pl>
2022-05-28 13:50:42 +0200

    Add new `NavigationSeparator` control [#173][skip ci]

---
+++ src/WPFUI/Controls/NavigationSeparator.cs
@@ -0,0 +1,12 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using WPFUI.Controls.Interfaces;
+
+namespace WPFUI.Controls;
+
+public class NavigationSeparator : System.Windows.Controls.Separator, INavigationControl
+{
+}
