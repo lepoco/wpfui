7d67888c8e83afd147aa496ea15b5b5220426ca0
pomianowski <kontakt@rapiddev.pl>
2022-05-28 13:52:47 +0200

    Add custom navigation service item [skip ci]

---
+++ src/WPFUI/Controls/Navigation/NavigationServiceItem.cs
@@ -0,0 +1,32 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using System;
+using WPFUI.Controls.Interfaces;
+
+namespace WPFUI.Controls.Navigation;
+
+/// <summary>
+/// Represents <see cref="Interfaces.INavigationItem"/> in <see cref="NavigationService"/>.
+/// </summary>
+internal class NavigationServiceItem
+{
+    public string Tag { get; set; } = String.Empty;
+    public bool Cache { get; set; } = false;
+    public Type Type { get; set; } = (Type)null;
+    public Uri Source { get; set; } = (Uri)null;
+    public object Instance { get; set; } = null;
+
+    public static NavigationServiceItem Get(INavigationItem navigationItem)
+    {
+        return new NavigationServiceItem
+        {
+            Tag = navigationItem.PageTag,
+            Type = navigationItem.PageType,
+            Source = navigationItem.AbsolutePageSource,
+            Cache = navigationItem.Cache
+        };
+    }
+}
