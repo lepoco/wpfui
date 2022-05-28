5a498adfa8f9397f67844683ea115c51e8618baf
pomianowski <kontakt@rapiddev.pl>
2022-05-28 13:50:20 +0200

    Add new `NavigationHeader` control [#173[skip ci]

---
+++ src/WPFUI/Controls/NavigationHeader.cs
@@ -0,0 +1,94 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media;
+using WPFUI.Controls.Interfaces;
+
+namespace WPFUI.Controls;
+
+public class NavigationHeader : System.Windows.Controls.Control, INavigationControl, IIconControl
+{
+    /// <summary>
+    /// Property for <see cref="Text"/>.
+    /// </summary>
+    public static readonly DependencyProperty TextProperty = DependencyProperty.Register(nameof(Text),
+        typeof(string), typeof(NavigationHeader),
+        new PropertyMetadata(String.Empty));
+
+    /// <summary>
+    /// Property for <see cref="Icon"/>.
+    /// </summary>
+    public static readonly DependencyProperty IconProperty = DependencyProperty.Register(nameof(Icon),
+        typeof(Common.SymbolRegular), typeof(NavigationHeader),
+        new PropertyMetadata(Common.SymbolRegular.Empty));
+
+    /// <summary>
+    /// Property for <see cref="IconFilled"/>.
+    /// </summary>
+    public static readonly DependencyProperty IconFilledProperty = DependencyProperty.Register(nameof(IconFilled),
+        typeof(bool), typeof(NavigationHeader), new PropertyMetadata(false));
+
+    /// <summary>
+    /// Property for <see cref="IconForeground"/>.
+    /// </summary>
+    public static readonly DependencyProperty IconForegroundProperty = DependencyProperty.Register(nameof(IconForeground),
+        typeof(Brush), typeof(NavigationHeader), new FrameworkPropertyMetadata(SystemColors.ControlTextBrush,
+            FrameworkPropertyMetadataOptions.Inherits));
+
+    /// <summary>
+    /// Property for <see cref="IconSize"/>.
+    /// </summary>
+    public static readonly DependencyProperty IconSizeProperty = DependencyProperty.Register(nameof(IconSize),
+        typeof(double), typeof(NavigationHeader), new FrameworkPropertyMetadata(13d));
+
+    /// <summary>
+    /// Text presented in the header element.
+    /// </summary>
+    [Bindable(true)]
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+    /// <inheritdoc />
+    [Bindable(true), Category("Appearance")]
+    public Common.SymbolRegular Icon
+    {
+        get => (Common.SymbolRegular)GetValue(IconProperty);
+        set => SetValue(IconProperty, value);
+    }
+
+    /// <inheritdoc />
+    [Bindable(true), Category("Appearance")]
+    public bool IconFilled
+    {
+        get => (bool)GetValue(IconFilledProperty);
+        set => SetValue(IconFilledProperty, value);
+    }
+
+    /// <summary>
+    /// Foreground of the <see cref="WPFUI.Controls.SymbolIcon"/>.
+    /// </summary>
+    [Bindable(true), Category("Appearance")]
+    public Brush IconForeground
+    {
+        get => (Brush)GetValue(IconForegroundProperty);
+        set => SetValue(IconForegroundProperty, value);
+    }
+
+    /// <summary>
+    /// Font size of the <see cref="WPFUI.Controls.SymbolIcon"/>.
+    /// </summary>
+    [Bindable(true), Category("Appearance")]
+    public double IconSize
+    {
+        get => (double)GetValue(IconSizeProperty);
+        set => SetValue(IconSizeProperty, value);
+    }
+}
