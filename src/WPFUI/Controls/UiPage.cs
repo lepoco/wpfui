7a485f98fc03fe0a973165f8be3868e71f023723
pomianowski <kontakt@rapiddev.pl>
2022-05-30 18:33:01 +0200

    Allow interaction with template ScrollViewer of `UiPage` [#195][skip ci]

--- src/WPFUI/Controls/UiPage.cs
+++ src/WPFUI/Controls/UiPage.cs
@@ -3,21 +3,35 @@
 // Copyright (C) Leszek Pomianowski and WPF UI Contributors.
 // All Rights Reserved.
 
+using System;
 using System.ComponentModel;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace WPFUI.Controls;
 
 /// <summary>
 /// Extended <see cref="System.Windows.Controls.Page"/> with WPF UI features. 
 /// </summary>
+[TemplatePart(Name = "PART_ScrollViewer", Type = typeof(WPFUI.Controls.DynamicScrollViewer))]
 public class UiPage : System.Windows.Controls.Page
 {
+    /// <summary>
+    /// Template element represented by the <c>PART_Popup</c> name.
+    /// </summary>
+    private const string ElementScrollViewer = "PART_ScrollViewer";
+
     /// <summary>
     /// Property for <see cref="Scrollable"/>.
     /// </summary>
     public static readonly DependencyProperty ScrollableProperty = DependencyProperty.Register(nameof(Scrollable),
         typeof(bool), typeof(UiPage), new PropertyMetadata(false));
+
+    /// <summary>
+    /// Property for <see cref="ScrollHost"/>.
+    /// </summary>
+    public static readonly DependencyProperty ScrollHostProperty = DependencyProperty.Register(nameof(ScrollHost),
+        typeof(ScrollViewer), typeof(UiPage), new PropertyMetadata((ScrollViewer)null));
 
     /// <summary>
     /// Gets or sets a value determining whether the content should be scrollable.
@@ -30,6 +44,22 @@ public class UiPage : System.Windows.Controls.Page
         set => SetValue(ScrollableProperty, value);
     }
 
+    /// <summary>
+    /// If the page is <see cref="Scrollable"/>, gets a <see cref="ScrollViewer"/> that container the <see cref="Page"/>.
+    /// </summary>
+    [Bindable(false)]
+    public ScrollViewer ScrollHost
+    {
+        get
+        {
+            if (!Scrollable)
+                throw new InvalidOperationException($"{nameof(ScrollHost)} can only be taken from a page that has {nameof(Scrollable)} set to true.");
+
+            return (ScrollViewer)GetValue(ScrollHostProperty);
+        }
+        private set => SetValue(ScrollHostProperty, value);
+    }
+
     public UiPage()
     {
         SetResourceReference(StyleProperty, typeof(UiPage));
@@ -38,5 +68,19 @@ public class UiPage : System.Windows.Controls.Page
     static UiPage()
     {
         DefaultStyleKeyProperty.OverrideMetadata(typeof(UiPage), new FrameworkPropertyMetadata(typeof(UiPage)));
+    }
+
+    /// <summary>
+    /// Invoked whenever application code or an internal process,
+    /// such as a rebuilding layout pass, calls the ApplyTemplate method.
+    /// </summary>
+    public override void OnApplyTemplate()
+    {
+        base.OnApplyTemplate();
+
+        var scrollHost = GetTemplateChild(ElementScrollViewer);
+
+        if (scrollHost is ScrollViewer)
+            ScrollHost = scrollHost as ScrollViewer;
     }
 }
