aff46e8aed99b414c130587ee76d129de8011a93
pomianowski <kontakt@rapiddev.pl>
2022-06-03 19:51:58 +0200

    Keep code style for `Resources` class

--- src/WPFUI/Resources.cs
+++ src/WPFUI/Resources.cs
@@ -8,85 +8,84 @@ using System.Windows;
 using System.Windows.Media;
 using WPFUI.Appearance;
 
-namespace WPFUI
+namespace WPFUI;
+
+/// <summary>
+/// Facilitates convenient management of WPF UI resources in the application that uses it.
+/// </summary>
+public class Resources : System.Windows.ResourceDictionary
 {
+    private static bool _themeInitialized = false;
+
+    private static bool _accentInitialized = false;
+
     /// <summary>
-    /// Facilitates convenient management of WPF UI resources in the application that uses it.
+    /// Gets or sets the application theme.
     /// </summary>
-    public class Resources : System.Windows.ResourceDictionary
+    public ThemeType Theme
     {
-        private static bool _themeInitialized = false;
+        set => InitializeTheme(value);
+    }
 
-        private static bool _accentInitialized = false;
+    /// <summary>
+    /// Gets or sets the application accent color.
+    /// </summary>
+    public Color Accent
+    {
+        set => InitializeAccent(value);
+    }
 
-        /// <summary>
-        /// Gets or sets the application theme.
-        /// </summary>
-        public ThemeType Theme
-        {
-            set => InitializeTheme(value);
-        }
+    /// <summary>
+    /// Initializes library resources.
+    /// </summary>
+    protected void InitializeTheme(ThemeType themeType)
+    {
+        if (_themeInitialized)
+            return;
 
-        /// <summary>
-        /// Gets or sets the application accent color.
-        /// </summary>
-        public Color Accent
-        {
-            set => InitializeAccent(value);
-        }
-
-        /// <summary>
-        /// Initializes library resources.
-        /// </summary>
-        protected void InitializeTheme(ThemeType themeType)
-        {
-            if (_themeInitialized)
-                return;
-
-            _themeInitialized = true;
+        _themeInitialized = true;
 #if DEBUG
-            System.Diagnostics.Debug.WriteLine($"INFO | {typeof(Resources)} initialized, selected theme: {themeType}",
-                "WPFUI.Resources");
+        System.Diagnostics.Debug.WriteLine($"INFO | {typeof(Resources)} initialized, selected theme: {themeType}",
+            "WPFUI.Resources");
 #endif
-            MergedDictionaries.Add(new ResourceDictionary { Source = GetResourceUri("WPFUI") });
-            MergedDictionaries.Add(new ResourceDictionary { Source = GetResourceUri(GetThemeResourceName(themeType)) });
-        }
+        MergedDictionaries.Add(new ResourceDictionary { Source = GetResourceUri("WPFUI") });
+        MergedDictionaries.Add(new ResourceDictionary { Source = GetResourceUri(GetThemeResourceName(themeType)) });
+    }
 
-        /// <summary>
-        /// Tries to apply accent color to the application controls.
-        /// </summary>
-        protected void InitializeAccent(Color accent)
-        {
-            if (_accentInitialized)
-                return;
+    /// <summary>
+    /// Tries to apply accent color to the application controls.
+    /// </summary>
+    protected void InitializeAccent(Color accent)
+    {
+        if (_accentInitialized)
+            return;
 
-            _accentInitialized = true;
+        _accentInitialized = true;
 #if DEBUG
-            System.Diagnostics.Debug.WriteLine($"INFO | {typeof(Resources)} initialized, selected accent: {accent}",
-                "WPFUI.Resources");
+        System.Diagnostics.Debug.WriteLine($"INFO | {typeof(Resources)} initialized, selected accent: {accent}",
+            "WPFUI.Resources");
 #endif
-            // TODO: ...
-        }
+        // TODO: ...
+    }
 
-        /// <summary>
-        /// Gets absolute path to the library resource.
-        /// </summary>
-        protected Uri GetResourceUri(string path)
-        {
-            return new Uri($"pack://application:,,,/WPFUI;component/Styles/{path}.xaml");
-        }
+    /// <summary>
+    /// Gets absolute path to the library resource.
+    /// </summary>
+    protected Uri GetResourceUri(string path)
+    {
+        return new Uri($"pack://application:,,,/WPFUI;component/Styles/{path}.xaml");
+    }
 
-        /// <summary>
-        /// Gets path to the selected theme resource.
-        /// </summary>
-        protected string GetThemeResourceName(ThemeType themeType)
+    /// <summary>
+    /// Gets path to the selected theme resource.
+    /// </summary>
+    protected string GetThemeResourceName(ThemeType themeType)
+    {
+        return themeType switch
         {
-            return themeType switch
-            {
-                ThemeType.HighContrast => "Theme/HighContrast",
-                ThemeType.Light => "Theme/Light",
-                _ => "Theme/Dark"
-            };
-        }
+            ThemeType.HighContrast => "Theme/HighContrast",
+            ThemeType.Light => "Theme/Light",
+            _ => "Theme/Dark"
+        };
     }
 }
