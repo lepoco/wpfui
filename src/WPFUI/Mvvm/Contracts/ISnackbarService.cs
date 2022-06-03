baca2677a94de3e7341fd64781c98c816c1d2862
pomianowski <kontakt@rapiddev.pl>
2022-06-03 19:57:33 +0200

    Initially define Snackbar contract

---
+++ src/WPFUI/Mvvm/Contracts/ISnackbarService.cs
@@ -0,0 +1,22 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using WPFUI.Common;
+
+namespace WPFUI.Mvvm.Contracts;
+
+/// <summary>
+/// Represents a contract with the service that provides global <see cref="WPFUI.Controls.Snackbar"/>.
+/// </summary>
+public interface ISnackbarService
+{
+    void ShowSnackbar(string title);
+
+    void ShowSnackbar(string title, string message);
+
+    void ShowSnackbar(string title, string message, SymbolRegular icon);
+
+    void CloseSnackbar();
+}
