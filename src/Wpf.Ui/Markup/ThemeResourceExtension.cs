a0a92ca9292e1abdf3c45ead0ce51540da1275a8
pomianowski <kontakt@rapiddev.pl>
2022-07-02 16:36:10 +0200

    Refactor ThemeResource markup extensions

--- src/Wpf.Ui/Markup/ThemeResourceExtension.cs
+++ src/Wpf.Ui/Markup/ThemeResourceExtension.cs
@@ -4,30 +4,25 @@
 // All Rights Reserved.
 
 using System;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Markup;
 
 namespace Wpf.Ui.Markup;
 
 /// <summary>
-/// Class for Xaml markup extension for static theme resource references.
+///  Class for Xaml markup extension for static resource references.
 /// </summary>
+[TypeConverter(typeof(DynamicResourceExtensionConverter))]
 [MarkupExtensionReturnType(typeof(object))]
-public class ThemeResourceExtension : MarkupExtension
+public class ThemeResourceExtension : DynamicResourceExtension
 {
-    /// <summary>
-    ///  The key in a Resource Dictionary used to find the object referred to by this
-    ///  Markup Extension.
-    /// </summary>
-    [ConstructorArgument("resourceKey")] // Uses an instance descriptor
-    public ThemeResource ResourceKey { get; set; }
-
     /// <summary>
     ///  Constructor that takes no parameters
     /// </summary>
     public ThemeResourceExtension()
     {
-        ResourceKey = ThemeResource.Unknown;
+        ResourceKey = ThemeResource.ApplicationBackgroundBrush.ToString();
     }
 
     /// <summary>
@@ -35,34 +30,9 @@ public class ThemeResourceExtension : MarkupExtension
     /// </summary>
     public ThemeResourceExtension(ThemeResource resourceKey)
     {
-        ResourceKey = resourceKey;
-    }
+        if (resourceKey == ThemeResource.Unknown)
+            throw new ArgumentNullException(nameof(resourceKey));
 
-    /// <summary>
-    ///  Return an object that should be set on the targetObject's targetProperty
-    ///  for this markup extension.  For DynamicResourceExtension, this is the object found in
-    ///  a resource dictionary in the current parent chain that is keyed by ResourceKey
-    /// </summary>
-    /// <returns>
-    ///  The object to set on this property.
-    /// </returns>
-    public override object ProvideValue(IServiceProvider serviceProvider)
-    {
-        if (ResourceKey == ThemeResource.Unknown)
-            throw new InvalidOperationException("Missing theme resource key.");
-
-        if (serviceProvider != null)
-        {
-            // DynamicResourceExtensions are not allowed On CLR props except for Setter,Trigger,Condition (bugs 1183373,1572537)
-
-            DependencyObject targetDependencyObject;
-            DependencyProperty targetDependencyProperty;
-
-            //Helper.CheckCanReceiveMarkupExtension(this, serviceProvider, out targetDependencyObject, out targetDependencyProperty);
-        }
-
-        var applicationResource = Application.Current.Resources[ResourceKey.ToString()];
-
-        return applicationResource;
+        ResourceKey = resourceKey.ToString();
     }
 }
