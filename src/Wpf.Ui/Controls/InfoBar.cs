ea6c423713130eefb114b2515b4d7cd24be8ee26
Sören Kuklau <chucker23n+github@gmail.com>
2022-09-20 18:24:44 +0200

    features/InfoBar: wip

---
+++ src/Wpf.Ui/Controls/InfoBar.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+
+namespace Wpf.Ui.Controls
+{
+    public class InfoBar : System.Windows.Controls.ContentControl
+    {
+
+        public static readonly DependencyProperty IsOpenProperty =
+            DependencyProperty.Register("IsOpen", typeof(bool), typeof(InfoBar),
+                new PropertyMetadata(false));
+
+        public static readonly DependencyProperty MessageProperty =
+            DependencyProperty.Register("Message", typeof(string), typeof(InfoBar),
+                new PropertyMetadata(""));
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(InfoBar),
+                new PropertyMetadata(""));
+
+        public bool IsOpen
+        {
+            get { return (bool)GetValue(IsOpenProperty); }
+            set { SetValue(IsOpenProperty, value); }
+        }
+
+        public string Message
+        {
+            get => (string)GetValue(MessageProperty);
+            set => SetValue(MessageProperty, value);
+        }
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+    }
+}
