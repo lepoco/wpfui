4db259190deb6a56dc07c22c249a81a7be40fd60
pomianowski <kontakt@rapiddev.pl>
2023-01-06 11:36:50 +0100

    Drop validation rules

--- src/Wpf.Ui.Gallery/ViewModels/Windows/MainWindowViewModel.cs
+++ src/Wpf.Ui.Gallery/ViewModels/Windows/MainWindowViewModel.cs
@@ -9,6 +9,7 @@ using System.Collections.ObjectModel;
 using System.Windows;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Wpf.Ui.Common;
+using Wpf.Ui.Controls;
 using Wpf.Ui.Controls.Navigation;
 using Wpf.Ui.Gallery.Views.Pages;
 using Wpf.Ui.Gallery.Views.Pages.BasicInput;
@@ -44,10 +45,10 @@ public partial class MainWindowViewModel : ObservableObject
 
         _menuItems = new ObservableCollection<object>
         {
-            new NavigationViewItem { Content = "Home", Icon = SymbolRegular.Home24, TargetPageType = typeof(DashboardPage) },
-            new NavigationViewItem { Content = "All Controls", Icon = SymbolRegular.List24, TargetPageType = typeof(AllControlsPage) },
+            new NavigationViewItem { Content = "Home", Icon = new SymbolIcon { Symbol = SymbolRegular.Home24  } , TargetPageType = typeof(DashboardPage) },
+            new NavigationViewItem { Content = "All Controls", Icon = new SymbolIcon { Symbol = SymbolRegular.List24  }, TargetPageType = typeof(AllControlsPage) },
             new NavigationViewItemSeparator(),
-            new NavigationViewItem {Content = "Basic input", Icon = SymbolRegular.CheckboxChecked24, TargetPageType = typeof(BasicInputPage), MenuItems = new ObservableCollection<object>
+            new NavigationViewItem {Content = "Basic input", Icon = new SymbolIcon { Symbol = SymbolRegular.CheckboxChecked24  }, TargetPageType = typeof(BasicInputPage), MenuItems = new ObservableCollection<object>
             {
                 new NavigationViewItem { Content = "Anchor", TargetPageType = typeof(AnchorPage) },
                 new NavigationViewItem { Content = "Button", TargetPageType = typeof(ButtonPage) },
@@ -61,7 +62,7 @@ public partial class MainWindowViewModel : ObservableObject
                 new NavigationViewItem { Content = "ThumbRate", TargetPageType = typeof(ThumbRatePage) },
                 new NavigationViewItem { Content = "Slider", TargetPageType = typeof(SliderPage) },
             }},
-            new NavigationViewItem {Content = "Collections", Icon = SymbolRegular.Table24, TargetPageType = typeof(CollectionsPage), MenuItems = new ObservableCollection<object>
+            new NavigationViewItem {Content = "Collections", Icon = new SymbolIcon { Symbol = SymbolRegular.Table24  }, TargetPageType = typeof(CollectionsPage), MenuItems = new ObservableCollection<object>
             {
                 new NavigationViewItem { Content = "DataGrid", TargetPageType = typeof(DataGridPage) },
                 new NavigationViewItem { Content = "ListBox", TargetPageType = typeof(ListBoxPage) },
@@ -71,37 +72,37 @@ public partial class MainWindowViewModel : ObservableObject
                 new NavigationViewItem { Content = "TreeList", TargetPageType = typeof(TreeListPage) },
 #endif
             }},
-            new NavigationViewItem {Content = "Date and Time", Icon = SymbolRegular.CalendarClock24, TargetPageType = typeof(DateAndTimePage), MenuItems = new ObservableCollection<object>
+            new NavigationViewItem {Content = "Date and Time", Icon = new SymbolIcon { Symbol = SymbolRegular.CalendarClock24  }, TargetPageType = typeof(DateAndTimePage), MenuItems = new ObservableCollection<object>
             {
                 new NavigationViewItem { Content = "Calendar", TargetPageType = typeof(CalendarPage) },
                 new NavigationViewItem { Content = "DatePicker", TargetPageType = typeof(DatePickerPage) },
             }},
-            new NavigationViewItem {Content = "Dialogs and Flyouts", Icon = SymbolRegular.Chat24, TargetPageType = typeof(DialogsAndFlyoutsPage), MenuItems = new ObservableCollection<object>
+            new NavigationViewItem {Content = "Dialogs and Flyouts", Icon = new SymbolIcon { Symbol = SymbolRegular.Chat24  }, TargetPageType = typeof(DialogsAndFlyoutsPage), MenuItems = new ObservableCollection<object>
             {
                 new NavigationViewItem { Content = "Snackbar", TargetPageType = typeof(SnackbarPage) },
                 new NavigationViewItem { Content = "Dialog", TargetPageType = typeof(DialogPage) },
                 new NavigationViewItem { Content = "Flyout", TargetPageType = typeof(FlyoutPage) },
                 new NavigationViewItem { Content = "MessageBox", TargetPageType = typeof(MessageBoxPage) },
             }},
-            new NavigationViewItem {Content = "Media", Icon = SymbolRegular.PlayCircle24, TargetPageType = typeof(MediaPage), MenuItems = new ObservableCollection<object>
+            new NavigationViewItem {Content = "Media", Icon = new SymbolIcon { Symbol = SymbolRegular.PlayCircle24  }, TargetPageType = typeof(MediaPage), MenuItems = new ObservableCollection<object>
             {
                 new NavigationViewItem { Content = "Image", TargetPageType = typeof(ImagePage) },
                 new NavigationViewItem { Content = "WebView", TargetPageType = typeof(WebViewPage) },
                 new NavigationViewItem { Content = "WebBrowser", TargetPageType = typeof(WebBrowserPage) },
             }},
-            new NavigationViewItem {Content = "Navigation", Icon = SymbolRegular.Navigation24, TargetPageType = typeof(NavigationPage), MenuItems = new ObservableCollection<object>
+            new NavigationViewItem {Content = "Navigation", Icon = new SymbolIcon { Symbol = SymbolRegular.Navigation24  }, TargetPageType = typeof(NavigationPage), MenuItems = new ObservableCollection<object>
             {
                 new NavigationViewItem { Content = "NavigationView", TargetPageType = typeof(NavigationViewPage) },
                 new NavigationViewItem { Content = "TabControl", TargetPageType = typeof(TabControlPage) },
             }},
-            new NavigationViewItem {Content = "Status and Info", Icon = SymbolRegular.ChatBubblesQuestion24, TargetPageType = typeof(StatusAndInfoPage), MenuItems = new ObservableCollection<object>
+            new NavigationViewItem {Content = "Status and Info", Icon = new SymbolIcon { Symbol = SymbolRegular.ChatBubblesQuestion24  }, TargetPageType = typeof(StatusAndInfoPage), MenuItems = new ObservableCollection<object>
             {
                 new NavigationViewItem { Content = "InfoBar", TargetPageType = typeof(InfoBarPage) },
                 new NavigationViewItem { Content = "ProgressBar", TargetPageType = typeof(ProgressBarPage) },
                 new NavigationViewItem { Content = "ProgressRing", TargetPageType = typeof(ProgressRingPage) },
                 new NavigationViewItem { Content = "ToolTip", TargetPageType = typeof(ToolTipPage) },
             }},
-            new NavigationViewItem {Content = "Text", Icon = SymbolRegular.DrawText24, TargetPageType = typeof(TextPage), MenuItems = new ObservableCollection<object>
+            new NavigationViewItem {Content = "Text", Icon = new SymbolIcon { Symbol = SymbolRegular.DrawText24  }, TargetPageType = typeof(TextPage), MenuItems = new ObservableCollection<object>
             {
                 new NavigationViewItem { Content = "AutoSuggestBox", TargetPageType = typeof(AutoSuggestBoxPage) },
                 new NavigationViewItem { Content = "NumberBox", TargetPageType = typeof(NumberBoxPage) },
@@ -111,7 +112,7 @@ public partial class MainWindowViewModel : ObservableObject
                 new NavigationViewItem { Content = "TextBlock", TargetPageType = typeof(TextBlockPage) },
                 new NavigationViewItem { Content = "TextBox", TargetPageType = typeof(TextBoxPage) },
             }},
-            new NavigationViewItem {Content = "Icons", Icon = SymbolRegular.Fluent24, TargetPageType = typeof(IconsPage), MenuItems = new ObservableCollection<object>
+            new NavigationViewItem {Content = "Icons", Icon = new SymbolIcon { Symbol = SymbolRegular.Fluent24  }, TargetPageType = typeof(IconsPage), MenuItems = new ObservableCollection<object>
             {
                 new NavigationViewItem { Content = "SymbolIcon", TargetPageType = typeof(SymbolIconPage) },
                 new NavigationViewItem { Content = "FontIcon", TargetPageType = typeof(FontIconPage) },
@@ -121,12 +122,12 @@ public partial class MainWindowViewModel : ObservableObject
         var toggleThemeNavigationViewItem = new NavigationViewItem
         {
             Content = "Toggle theme",
-            Icon = SymbolRegular.PaintBrush24
+            Icon = new SymbolIcon { Symbol = SymbolRegular.PaintBrush24 }
         };
         toggleThemeNavigationViewItem.Click += OnToggleThemeClicked;
 
         _footerMenuItems.Add(toggleThemeNavigationViewItem);
-        _footerMenuItems.Add(new NavigationViewItem { Content = "Settings", Icon = SymbolRegular.Settings24, TargetPageType = typeof(SettingsPage) });
+        _footerMenuItems.Add(new NavigationViewItem { Content = "Settings", Icon = new SymbolIcon { Symbol = SymbolRegular.Settings24 }, TargetPageType = typeof(SettingsPage) });
     }
 
     private void OnToggleThemeClicked(object sender, RoutedEventArgs e)
