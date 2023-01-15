19c23b576d8171ed6f11fda72abd6f32c43f0b90
Ivan Dmitriev <42055372+IvanDmitriev1@users.noreply.github.com>
2023-01-15 18:42:10 +0600

    added BreadcrumbBarPage in Gallery

--- src/Wpf.Ui.Gallery/ViewModels/Pages/Navigation/NavigationViewModel.cs
+++ src/Wpf.Ui.Gallery/ViewModels/Pages/Navigation/NavigationViewModel.cs
@@ -3,10 +3,7 @@
 // Copyright (C) Leszek Pomianowski and WPF UI Contributors.
 // All Rights Reserved.
 
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using CommunityToolkit.Mvvm.ComponentModel;
-using Wpf.Ui.Common;
 using Wpf.Ui.Gallery.Models;
 
 namespace Wpf.Ui.Gallery.ViewModels.Pages.Navigation;
@@ -16,6 +13,13 @@ public partial class NavigationViewModel : ObservableObject
     [ObservableProperty]
     private ICollection<NavigationCard> _navigationCards = new ObservableCollection<NavigationCard>
     {
+        new()
+        {
+            Name = "BreadcrumbBar",
+            Icon = SymbolRegular.Navigation24,
+            Description = "Shows the trail of navigation taken to the current location.",
+            Link = "BreadcrumbBar"
+        },
         new()
         {
             Name = "NavigationView",
