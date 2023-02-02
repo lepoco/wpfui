542f22f9097bcdd6550f9bf1442f27f6e14960d0
Ivan Dmitriev <42055372+IvanDmitriev1@users.noreply.github.com>
2023-02-02 22:22:41 +0600

    Added new properties

--- src/Wpf.Ui/Controls/AutoSuggestBox/NewAutoSuggestBox.cs
+++ src/Wpf.Ui/Controls/AutoSuggestBox/NewAutoSuggestBox.cs
@@ -21,9 +21,27 @@ public class NewAutoSuggestBox : System.Windows.Controls.ItemsControl
     /// <summary>
     /// Property for <see cref="IsSuggestionListOpen"/>.
     /// </summary>
-    public static readonly DependencyProperty IsSuggestionListOpenProperty = DependencyProperty.Register(nameof(IsSuggestionListOpen),
-        typeof(bool), typeof(NewAutoSuggestBox),
-        new PropertyMetadata(false));
+    public static readonly DependencyProperty IsSuggestionListOpenProperty =
+        DependencyProperty.Register(nameof(IsSuggestionListOpen), typeof(bool), typeof(NewAutoSuggestBox),
+            new PropertyMetadata(false));
+
+    /// <summary>
+    /// Property for <see cref="Text"/>.
+    /// </summary>
+    public static readonly DependencyProperty TextProperty = DependencyProperty.Register(nameof(Text), typeof(string), typeof(NewAutoSuggestBox),
+        new PropertyMetadata(string.Empty));
+
+    /// <summary>
+    /// Property for <see cref="PlaceholderText"/>.
+    /// </summary>
+    public static readonly DependencyProperty PlaceholderTextProperty = DependencyProperty.Register(nameof(PlaceholderText), typeof(string), typeof(NewAutoSuggestBox),
+        new PropertyMetadata(string.Empty));
+
+    /// <summary>
+    /// Property for <see cref="MaxSuggestionListHeight"/>.
+    /// </summary>
+    public static readonly DependencyProperty MaxSuggestionListHeightProperty = DependencyProperty.Register(nameof(MaxSuggestionListHeight), typeof(double), typeof(NewAutoSuggestBox),
+        new PropertyMetadata(0d));
 
     #endregion
 
@@ -36,6 +54,39 @@ public class NewAutoSuggestBox : System.Windows.Controls.ItemsControl
     {
         get => (bool)GetValue(IsSuggestionListOpenProperty);
         set => SetValue(IsSuggestionListOpenProperty, value);
+    }
+
+    /// <summary>
+    /// Gets or sets the text that is shown in the control
+    /// </summary>
+    /// <remarks>
+    /// This property is not typically set in XAML.
+    /// </remarks>
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+    /// <summary>
+    /// Gets or sets the placeholder text to be displayed in the control.
+    /// </summary>
+    /// <remarks>
+    /// The placeholder text to be displayed in the control. The default is an empty string.
+    /// </remarks>
+    public string PlaceholderText
+    {
+        get => (string)GetValue(PlaceholderTextProperty);
+        set => SetValue(PlaceholderTextProperty, value);
+    }
+
+    /// <summary>
+    /// Gets or set the maximum height for the drop-down portion of the <see cref="AutoSuggestBox"/> control.
+    /// </summary>
+    public double MaxSuggestionListHeight
+    {
+        get => (double)GetValue(MaxSuggestionListHeightProperty);
+        set => SetValue(MaxSuggestionListHeightProperty, value);
     }
 
     #endregion
@@ -98,8 +149,6 @@ public class NewAutoSuggestBox : System.Windows.Controls.ItemsControl
 
     private void SuggestionsListOnLostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
     {
-        Debug.WriteLine("Losing focus");
-
         if (e.NewFocus is ListViewItem)
             return;
 
@@ -108,6 +157,12 @@ public class NewAutoSuggestBox : System.Windows.Controls.ItemsControl
 
     private void TextBoxOnTextChanged(object sender, TextChangedEventArgs e)
     {
+        if (string.IsNullOrEmpty(TextBox.Text))
+        {
+            IsSuggestionListOpen = false;
+            return;
+        }
+
         IsSuggestionListOpen = true;
     }
 
@@ -116,11 +171,11 @@ public class NewAutoSuggestBox : System.Windows.Controls.ItemsControl
         if (e.Key is not Key.Enter)
             return;
 
-        var q = SuggestionsList.SelectedItem;
+        var item = SuggestionsList.SelectedItem;
     }
 
     private void SuggestionsListOnSelectionChanged(object sender, SelectionChangedEventArgs e)
     {
-        Debug.WriteLine($"Selected | {e.AddedItems[0]}");
+        
     }
 }
