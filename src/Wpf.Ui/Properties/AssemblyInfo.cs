e5753ff786745318e1d3b444eca530fac6213e4f
pomianowski <kontakt@rapiddev.pl>
2023-02-03 13:35:44 +0100

    Update demo apps windows

--- src/Wpf.Ui/Properties/AssemblyInfo.cs
+++ src/Wpf.Ui/Properties/AssemblyInfo.cs
@@ -4,10 +4,11 @@
 // All Rights Reserved.
 
 using System.Runtime.InteropServices;
+using System.Windows;
 using System.Windows.Markup;
 
 [assembly: ComVisible(false)]
-
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 [assembly: Guid("072fb71f-784e-4113-9da0-a506ff1a0cd5")]
 
 [assembly: XmlnsPrefix("http://schemas.lepo.co/wpfui/2022/xaml", "ui")]
