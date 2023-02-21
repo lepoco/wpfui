7674ae8a283224411ae6327cbbb8b0c4d5c7b518
Ivan Dmitriev <42055372+IvanDmitriev1@users.noreply.github.com>
2023-02-21 13:30:53 +0600

    Added ImageIcon

---
+++ src/Wpf.Ui/Controls/IconElements/ImageIcon.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows.Media;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Wpf.Ui.Controls.IconElements;
+
+public class ImageIcon : IconElement
+{
+    /// <summary>
+    /// Gets/Sets the Source on this Image.
+    /// The Source property is the ImageSource that holds the actual image drawn.
+    /// </summary>
+    public static readonly DependencyProperty SourceProperty =
+        DependencyProperty.Register(nameof(Source), typeof(ImageSource), typeof(ImageIcon),
+            new FrameworkPropertyMetadata(null,
+                FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender, OnSourcePropertyChanged));
+
+    /// <summary>
+    /// Gets/Sets the Source on this Image.
+    /// The Source property is the ImageSource that holds the actual image drawn.
+    /// </summary>
+    public ImageSource? Source
+    {
+        get => (ImageSource)GetValue(SourceProperty);
+        set => SetValue(SourceProperty, value);
+    }
+
+    private System.Windows.Controls.Image? _image;
+
+    private static void OnSourcePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var self = (ImageIcon)d;
+        if (self._image is null)
+            return;
+
+        self._image.Source = (ImageSource)e.NewValue;
+    }
+
+    protected override void InitializeChildren()
+    {
+        _image = new System.Windows.Controls.Image()
+        {
+            Source = Source,
+            Stretch = Stretch.UniformToFill
+        };
+
+        Children.Add(_image);
+    }
+}
