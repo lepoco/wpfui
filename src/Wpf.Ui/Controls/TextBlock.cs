a6d6f613dbff63c562c6f559fa88af2f51980464
Ivan Dmitriev <42055372+IvanDmitriev1@users.noreply.github.com>
2023-03-17 21:54:34 +0600

    Added TextBlock with FontTypography and Appearance

---
+++ src/Wpf.Ui/Controls/TextBlock.cs
@@ -0,0 +1,54 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using System.Windows;
+using Wpf.Ui.Common;
+using Wpf.Ui.Extensions;
+
+namespace Wpf.Ui.Controls;
+
+public class TextBlock : System.Windows.Controls.TextBlock
+{
+    /// <summary>
+    /// Property for <see cref="FontTypography"/>.
+    /// </summary>
+    public static readonly DependencyProperty FontTypographyProperty = DependencyProperty.Register(
+        nameof(FontTypography),
+        typeof(FontTypography), typeof(TextBlock), new PropertyMetadata(FontTypography.Body,
+            static (o, args) => ((TextBlock)o).OnFontTypographyChanged((FontTypography)args.NewValue)));
+
+    /// <summary>
+    /// Property for <see cref="Appearance"/>.
+    /// </summary>
+    public static readonly DependencyProperty AppearanceProperty = DependencyProperty.Register(
+        nameof(Appearance),
+        typeof(TextColor), typeof(TextBlock), new PropertyMetadata(TextColor.Primary,
+            static (o, args) => ((TextBlock)o).OnAppearanceChanged((TextColor)args.NewValue)));
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public FontTypography FontTypography
+    {
+        get => (FontTypography)GetValue(FontTypographyProperty);
+        set => SetValue(FontTypographyProperty, value);
+    }
+
+    public TextColor Appearance
+    {
+        get => (TextColor)GetValue(AppearanceProperty);
+        set => SetValue(AppearanceProperty, value);
+    }
+
+    private void OnFontTypographyChanged(FontTypography newTypography)
+    {
+        SetResourceReference(StyleProperty, newTypography.ToResourceValue());
+    }
+
+    private void OnAppearanceChanged(TextColor textColor)
+    {
+        SetResourceReference(ForegroundProperty, textColor.ToResourceValue());
+    }
+}
