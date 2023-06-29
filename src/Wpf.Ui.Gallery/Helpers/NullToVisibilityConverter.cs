24e782e94f44b128c195096b76311c70b616e000
Ivan Dmitriev <42055372+IvanDmitriev1@users.noreply.github.com>
2023-06-29 10:51:50 +0600

    Added multi-line TextBox example

---
+++ src/Wpf.Ui.Gallery/Helpers/NullToVisibilityConverter.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Wpf.Ui.Gallery.Helpers;
+
+public class NullToVisibilityConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object parameter, CultureInfo culture) => value is null ? Visibility.Collapsed : Visibility.Visible;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+}
