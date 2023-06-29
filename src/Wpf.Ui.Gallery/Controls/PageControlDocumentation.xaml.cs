9aa0a31618083e6e0a9c7626bcb482920464e247
Ivan Dmitriev <42055372+IvanDmitriev1@users.noreply.github.com>
2023-06-29 11:35:08 +0600

    Added PageControlDocumentation

---
+++ src/Wpf.Ui.Gallery/Controls/PageControlDocumentation.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Wpf.Ui.Controls;
+
+namespace Wpf.Ui.Gallery.Controls;
+
+public class PageControlDocumentation : Control
+{
+    public static readonly DependencyProperty NavigationViewProperty = DependencyProperty.Register(
+        nameof(NavigationView),
+        typeof(INavigationView),
+        typeof(PageControlDocumentation),
+        new FrameworkPropertyMetadata(null)
+    );
+
+    public INavigationView? NavigationView
+    {
+        get => (INavigationView)GetValue(NavigationViewProperty);
+        set => SetValue(NavigationViewProperty, value);
+    }
+
+    public PageControlDocumentation()
+    {
+        Loaded += static (sender, _) => ((PageControlDocumentation)sender).OnLoaded();
+        Unloaded += static (sender, _) => ((PageControlDocumentation)sender).OnUnloaded();
+    }
+
+    private void OnLoaded()
+    {
+        if (NavigationView is null)
+            throw new ArgumentNullException(nameof(NavigationView));
+
+        NavigationView.Navigating += NavigationViewOnNavigating;
+    }
+
+    private void OnUnloaded()
+    {
+        NavigationView!.Navigating += NavigationViewOnNavigating;
+    }
+
+    private void NavigationViewOnNavigating(NavigationView sender, NavigatingCancelEventArgs args)
+    {
+        
+    }
+}
