8f93196e847bdb5bbb7fee1d4e9233b50fb3e9e6
Mohammad Amin Mollazadeh <madamin@pm.me>
2023-07-18 19:32:54 +0330

    feat(Wpf.Ui.Gallery): add CardControl

--- src/Wpf.Ui.Gallery/App.xaml.cs
+++ src/Wpf.Ui.Gallery/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿// This Source Code Form is subject to the terms of the MIT License.
+// This Source Code Form is subject to the terms of the MIT License.
 // If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
 // Copyright (C) Leszek Pomianowski and WPF UI Contributors.
 // All Rights Reserved.
@@ -148,6 +148,8 @@ public partial class App : Application
                 services.AddTransient<LayoutViewModel>();
                 services.AddTransient<ExpanderPage>();
                 services.AddTransient<ExpanderViewModel>();
+                services.AddTransient<CardControlPage>();
+                services.AddTransient<CardControlViewModel>();
 
                 // Web View
                 services.AddTransient<MediaPage>();
