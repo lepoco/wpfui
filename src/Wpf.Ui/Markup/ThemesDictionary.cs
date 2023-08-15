3bc1d7327ed01346da811f72c11e9f058523be5d
pomianowski <13592821+pomianowski@users.noreply.github.com>
2023-08-15 12:37:13 +0200

    Set default theme #725

--- src/Wpf.Ui/Markup/ThemesDictionary.cs
+++ src/Wpf.Ui/Markup/ThemesDictionary.cs
@@ -1,4 +1,4 @@
-﻿// This Source Code Form is subject to the terms of the MIT License.
+// This Source Code Form is subject to the terms of the MIT License.
 // If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
 // Copyright (C) Leszek Pomianowski and WPF UI Contributors.
 // All Rights Reserved.
@@ -38,4 +38,10 @@ public class ThemesDictionary : ResourceDictionary
             );
         }
     }
+
+    public ThemesDictionary() =>
+        Source = new Uri(
+            $"{AppearanceData.LibraryThemeDictionariesUri}Light.xaml",
+            UriKind.Absolute
+        );
 }
