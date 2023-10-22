554955ef1d8f24186912f855170809f595d4f6dc
pomian <13592821+pomianowski@users.noreply.github.com>
2023-10-22 15:34:11 +0200

    Disable ToolboxItem features

--- src/Wpf.Ui/Controls/ToggleSwitch/ToggleSwitch.cs
+++ src/Wpf.Ui/Controls/ToggleSwitch/ToggleSwitch.cs
@@ -12,8 +12,8 @@ namespace Wpf.Ui.Controls;
 /// <summary>
 /// Use <see cref="ToggleSwitch"/> to present users with two mutally exclusive options (like on/off).
 /// </summary>
-[ToolboxItem(true)]
-[ToolboxBitmap(typeof(ToggleSwitch), "ToggleSwitch.bmp")]
+// [ToolboxItem(true)]
+// [ToolboxBitmap(typeof(ToggleSwitch), "ToggleSwitch.bmp")]
 public class ToggleSwitch : System.Windows.Controls.Primitives.ToggleButton
 {
     /// <summary>
