5dd8c840a5208bc9bf541d35089f873393316677
Lukáš Rendvanský <lukas.rendvansky@outlook.com>
2023-11-09 14:32:52 +0100

    Add AutoSuggestBox internal event resubscriptions to Loaded event.
    This makes events to resubscribe each time component is loaded after unloading (which unsubscribes from events), not only once in `OnApplyTemplate()` method. Also check for Hwnd hook resubscription. Fixes #813.

--- src/Wpf.Ui/Controls/AutoSuggestBox/AutoSuggestBox.cs
+++ src/Wpf.Ui/Controls/AutoSuggestBox/AutoSuggestBox.cs
@@ -263,8 +263,17 @@ public class AutoSuggestBox : System.Windows.Controls.ItemsControl, IIconControl
 
     private object? _selectedItem;
 
+    private bool? _isHwndHookSubscribed;
+
     public AutoSuggestBox()
     {
+        Loaded += static (sender, _) =>
+        {
+            var self = (AutoSuggestBox)sender;
+
+            self.AcquireTemplateResources();
+        };
+
         Unloaded += static (sender, _) =>
         {
             var self = (AutoSuggestBox)sender;
@@ -282,18 +291,9 @@ public class AutoSuggestBox : System.Windows.Controls.ItemsControl, IIconControl
         TextBox = GetTemplateChild<TextBox>(ElementTextBox);
         SuggestionsPopup = GetTemplateChild<Popup>(ElementSuggestionsPopup);
         SuggestionsList = GetTemplateChild<ListView>(ElementSuggestionsList);
+        _isHwndHookSubscribed = false;
 
-        TextBox.PreviewKeyDown += TextBoxOnPreviewKeyDown;
-        TextBox.TextChanged += TextBoxOnTextChanged;
-        TextBox.LostKeyboardFocus += TextBoxOnLostKeyboardFocus;
-
-        SuggestionsList.SelectionChanged += SuggestionsListOnSelectionChanged;
-        SuggestionsList.PreviewKeyDown += SuggestionsListOnPreviewKeyDown;
-        SuggestionsList.LostKeyboardFocus += SuggestionsListOnLostKeyboardFocus;
-        SuggestionsList.PreviewMouseLeftButtonUp += SuggestionsListOnPreviewMouseLeftButtonUp;
-
-        var hwnd = (HwndSource)PresentationSource.FromVisual(this)!;
-        hwnd.AddHook(Hook);
+        AcquireTemplateResources();
     }
 
     /// <inheritdoc cref="UIElement.Focus" />
@@ -313,6 +313,40 @@ public class AutoSuggestBox : System.Windows.Controls.ItemsControl, IIconControl
         return dependencyObject;
     }
 
+    protected virtual void AcquireTemplateResources()
+    {
+        // Unsubscribe each handler before subscription, to prevent memory leak from double subscriptions.
+        // Unsubscription is safe, even if event has never been subscribed to.
+        if (TextBox != null)
+        {
+            TextBox.PreviewKeyDown -= TextBoxOnPreviewKeyDown;
+            TextBox.PreviewKeyDown += TextBoxOnPreviewKeyDown;
+            TextBox.TextChanged -= TextBoxOnTextChanged;
+            TextBox.TextChanged += TextBoxOnTextChanged;
+            TextBox.LostKeyboardFocus -= TextBoxOnLostKeyboardFocus;
+            TextBox.LostKeyboardFocus += TextBoxOnLostKeyboardFocus;
+        }
+
+        if (SuggestionsList != null)
+        {
+            SuggestionsList.SelectionChanged -= SuggestionsListOnSelectionChanged;
+            SuggestionsList.SelectionChanged += SuggestionsListOnSelectionChanged;
+            SuggestionsList.PreviewKeyDown -= SuggestionsListOnPreviewKeyDown;
+            SuggestionsList.PreviewKeyDown += SuggestionsListOnPreviewKeyDown;
+            SuggestionsList.LostKeyboardFocus -= SuggestionsListOnLostKeyboardFocus;
+            SuggestionsList.LostKeyboardFocus += SuggestionsListOnLostKeyboardFocus;
+            SuggestionsList.PreviewMouseLeftButtonUp -= SuggestionsListOnPreviewMouseLeftButtonUp;
+            SuggestionsList.PreviewMouseLeftButtonUp += SuggestionsListOnPreviewMouseLeftButtonUp;
+        }
+
+        if (_isHwndHookSubscribed.HasValue && !_isHwndHookSubscribed.Value)
+        {
+            var hwnd = (HwndSource)PresentationSource.FromVisual(this)!;
+            hwnd.AddHook(Hook);
+            _isHwndHookSubscribed = true;
+        }
+    }
+
     protected virtual void ReleaseTemplateResources()
     {
         if (TextBox != null)
@@ -330,9 +364,11 @@ public class AutoSuggestBox : System.Windows.Controls.ItemsControl, IIconControl
             SuggestionsList.PreviewMouseLeftButtonUp -= SuggestionsListOnPreviewMouseLeftButtonUp;
         }
 
-        if (PresentationSource.FromVisual(this) is HwndSource source)
+        if ((_isHwndHookSubscribed.HasValue && _isHwndHookSubscribed.Value)
+            && PresentationSource.FromVisual(this) is HwndSource source)
         {
             source.RemoveHook(Hook);
+            _isHwndHookSubscribed = false;
         }
     }
 
@@ -342,10 +378,7 @@ public class AutoSuggestBox : System.Windows.Controls.ItemsControl, IIconControl
     /// <param name="queryText">Currently submitted query text.</param>
     protected virtual void OnQuerySubmitted(string queryText)
     {
-        var args = new AutoSuggestBoxQuerySubmittedEventArgs(QuerySubmittedEvent, this)
-        {
-            QueryText = queryText
-        };
+        var args = new AutoSuggestBoxQuerySubmittedEventArgs(QuerySubmittedEvent, this) { QueryText = queryText };
 
         RaiseEvent(args);
     }
@@ -376,11 +409,7 @@ public class AutoSuggestBox : System.Windows.Controls.ItemsControl, IIconControl
     /// <param name="text">Changed text.</param>
     protected virtual void OnTextChanged(AutoSuggestionBoxTextChangeReason reason, string text)
     {
-        var args = new AutoSuggestBoxTextChangedEventArgs(TextChangedEvent, this)
-        {
-            Reason = reason,
-            Text = text
-        };
+        var args = new AutoSuggestBoxTextChangedEventArgs(TextChangedEvent, this) { Reason = reason, Text = text };
 
         RaiseEvent(args);
 
