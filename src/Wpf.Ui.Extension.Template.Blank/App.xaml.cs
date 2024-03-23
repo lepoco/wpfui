e99a9a87fe05e91609f1a8a4bbc7bb24be573618
pomianowski <13592821+pomianowski@users.noreply.github.com>
2024-03-23 14:45:36 +0100

    Update templates package version

--- src/Wpf.Ui.Extension.Template.Blank/App.xaml.cs
+++ src/Wpf.Ui.Extension.Template.Blank/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿// This Source Code Form is subject to the terms of the MIT License.
-// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
-// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
-// All Rights Reserved.
-
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using System.IO;
 using System.Reflection;
