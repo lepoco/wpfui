lepoco/wpfui

commit c0ace5b18648a2f7ccc77eff388cae1fa6ef8555

Analyzer Warning Mitigation (#1039)
* Fix null analyzer complaints: CS8602, CS8618, CS8603, CS8622, IDE0031

* Hide warnings related to IDE0008 related to using `var` keyword

* Removed Coersion logic from IconSourceElementConverter, and placed in IconElement and IcounSourceElement classes instead

* Cleaning single-line warnings: SA1005

- Removed commented-out Toolbox attributes
- Replaced single-line commented-out code with multi-line

* Fix missing braces or single-lined braces

- Add stylecop supression of 1502 "element should not be on a single line"
- Wrap with braces where needed

* Fix warnings on registered dependency properties

WPF0100, WPF0005, WPF0012

* Fix documentation warnings

WPF0108, WPF0060, SA1642, SA1623

* Fix warnings about using language keywords

- Use string, double, int rather than String, Double, Int32, etc
- remove 'this' keyword when context is obvious

* Fix warnings where attributes are on a single line SA1133

* When setting DPs, favor SetCurrentValue over the CLR setter. WPF0041

* Remove unnecessary things (parenthesis, assignments, etc). SA1119, IDE0059

* Fix order of operation warnings

* Do not use regions SA1124. Clean VirtualizingPanelBase of warnings

* Turn protected control fields into protected properties. fix some documentation warnings

* Cleanup warnings in Interop classes and suppress warnings related to uper-casing and private fields

* Continue cleaning up warnings in Wpf.Ui

* Edit .editorconfig to suppress warnings related to valueconverters

* Cleanup warning related to 'var'

* Fix warnings

* Split ContextMenu style from its loader dictionary

There were some issue when having a class called ContextMenu that wasn't actually a contextmenu but  a ResourceDictionary

* Resolve "expression value is never used" IDE0058 warnings

* Fix warnings related to collection initialization.

src/Wpf.Ui/Appearance/ApplicationThemeManager.cs
@@ -62,12 +62,10 @@ public static class ApplicationThemeManager
     /// <param name="applicationTheme">Theme to set.</param>
     /// <param name="backgroundEffect">Whether the custom background effect should be applied.</param>
     /// <param name="updateAccent">Whether the color accents should be changed.</param>
-    /// <param name="forceBackground">If <see langword="true"/>, bypasses the app's theme compatibility check and tries to force the change of a background effect.</param>
     public static void Apply(
         ApplicationTheme applicationTheme,
         WindowBackdropType backgroundEffect = WindowBackdropType.Mica,
-        bool updateAccent = true,
-        bool forceBackground = false
+        bool updateAccent = true
     )
     {
         if (updateAccent)
@@ -94,23 +92,14 @@ public static class ApplicationThemeManager
                 themeDictionaryName = "Dark";
                 break;
             case ApplicationTheme.HighContrast:
-                switch (ApplicationThemeManager.GetSystemTheme())
+                themeDictionaryName = ApplicationThemeManager.GetSystemTheme() switch
                 {
-                    case SystemTheme.HC1:
-                        themeDictionaryName = "HC1";
-                        break;
-                    case SystemTheme.HC2:
-                        themeDictionaryName = "HC2";
-                        break;
-                    case SystemTheme.HCBlack:
-                        themeDictionaryName = "HCBlack";
-                        break;
-                    case SystemTheme.HCWhite:
-                    default:
-                        themeDictionaryName = "HCWhite";
-                        break;
-                }
-
+                    SystemTheme.HC1 => "HC1",
+                    SystemTheme.HC2 => "HC2",
+                    SystemTheme.HCBlack => "HCBlack",
+                    SystemTheme.HCWhite => "HCBlack",
+                    _ => "HCWhite",
+                };
                 break;
         }
 
@@ -119,12 +108,11 @@ public static class ApplicationThemeManager
             new Uri(ThemesDictionaryPath + themeDictionaryName + ".xaml", UriKind.Absolute)
         );
 
-#if DEBUG
         System.Diagnostics.Debug.WriteLine(
             $"INFO | {typeof(ApplicationThemeManager)} tries to update theme to {themeDictionaryName} ({applicationTheme}): {isUpdated}",
             nameof(ApplicationThemeManager)
         );
-#endif
+
         if (!isUpdated)
         {
             return;
@@ -141,8 +129,7 @@ public static class ApplicationThemeManager
             WindowBackgroundManager.UpdateBackground(
                 mainWindow,
                 applicationTheme,
-                backgroundEffect,
-                forceBackground
+                backgroundEffect
             );
         }
     }

commit d73377660c0df092396e77e680b383f4af8b9b83

Fix incorrectly refactored High Contrast theme dictionary selection
This was broken in c0ace5b18648a2f7ccc77eff388cae1fa6ef8555

src/Wpf.Ui/Appearance/ApplicationThemeManager.cs
@@ -97,7 +97,7 @@ public static class ApplicationThemeManager
                     SystemTheme.HC1 => "HC1",
                     SystemTheme.HC2 => "HC2",
                     SystemTheme.HCBlack => "HCBlack",
-                    SystemTheme.HCWhite => "HCBlack",
+                    SystemTheme.HCWhite => "HCWhite",
                     _ => "HCWhite",
                 };
                 break;