6d77af93baa63fcc7df5b0ded580944677811611
Nice3point <nice3point@gmail.com>
2024-12-25 14:52:36 +0300

    Add String.Contains extension. Optimise string comparison

--- src/Wpf.Ui/Controls/GridView/GridViewRowPresenter.cs
+++ src/Wpf.Ui/Controls/GridView/GridViewRowPresenter.cs
@@ -3,9 +3,7 @@
 // Copyright (C) Leszek Pomianowski and WPF UI Contributors.
 // All Rights Reserved.
 
-using System.Reflection;
-using System.Windows.Controls;
-
+// ReSharper disable once CheckNamespace
 namespace Wpf.Ui.Controls;
 
 /// <summary>
