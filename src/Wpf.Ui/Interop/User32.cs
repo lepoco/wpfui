f8f7453cd6eae8f97035c3a3de3aa31d636c86a3
Alessio Doria <numero2405@gmail.com>
2025-03-04 16:40:58 +0100

    Update User32.cs

--- src/Wpf.Ui/Interop/User32.cs
+++ src/Wpf.Ui/Interop/User32.cs
@@ -1450,6 +1450,9 @@ internal static class User32
     [DllImport(Libraries.User32, SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
     public static extern bool GetCursorPos([Out] out WinDef.POINT lpPoint);
+    [DllImport(Libraries.User32), SetLastError = true)]
+	public static extern bool GetCursorPos(out System.Drawing.Point lpPoint);
+    
 
     [DllImport(Libraries.User32)]
     public static extern bool UnionRect(out WinDef.RECT rcDst, ref WinDef.RECT rc1, ref WinDef.RECT rc2);
