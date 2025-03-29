596fd05591beee29d257c25832627b7715613d17
dosymep <biseuv@yandex.ru>
2025-03-29 17:20:11 +0600

    create issue state

---
+++ samples/Wpf.Ui.Demo.SetResources.Simple/Models/DataGroup.cs
@@ -0,0 +1,20 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+namespace Wpf.Ui.Demo.SetResources.Simple.Models;
+
+public class DataGroup
+{
+    public DataGroup(bool selected, string name, string groupName)
+    {
+        Selected = selected;
+        Name = name;
+        GroupName = groupName;
+    }
+
+    public bool Selected { get; set; }
+    public string Name { get; set; }
+    public string GroupName { get; set; }
+}
--- samples/Wpf.Ui.Demo.SetResources.Simple/Views/Pages/ExpanderPage.xaml.cs
+++ samples/Wpf.Ui.Demo.SetResources.Simple/Views/Pages/ExpanderPage.xaml.cs
@@ -3,16 +3,37 @@
 // Copyright (C) Leszek Pomianowski and WPF UI Contributors.
 // All Rights Reserved.
 
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows.Controls;
+using System.Windows.Data;
+using Wpf.Ui.Demo.SetResources.Simple.Models;
 
 namespace Wpf.Ui.Demo.SetResources.Simple.Views.Pages;
 
-public partial class ExpanderPage : Page
+public partial class ExpanderPage
 {
+    public ObservableCollection<DataGroup> GroupCollection { get; private set; } = [];
+
     public ExpanderPage()
     {
         App.ApplyTheme(this);
-        
+
+        InitializeData();
         InitializeComponent();
+
+        ICollectionView collectionView = CollectionViewSource.GetDefaultView(GroupCollection);
+        collectionView.GroupDescriptions.Add(new PropertyGroupDescription(nameof(DataGroup.GroupName)));
+
+        Group.ItemsSource = collectionView;
+    }
+
+    private void InitializeData()
+    {
+        GroupCollection.Add(new DataGroup(false, "Audi", "Auto"));
+        GroupCollection.Add(new DataGroup(false, "Samsung S24 Ultra", "Phone"));
+        GroupCollection.Add(new DataGroup(true, "Apple iPhone 16 Pro", "Phone"));
+        GroupCollection.Add(new DataGroup(true, "Bugatti", "Auto"));
+        GroupCollection.Add(new DataGroup(false, "Lamborghini", "Auto"));
     }
 }
