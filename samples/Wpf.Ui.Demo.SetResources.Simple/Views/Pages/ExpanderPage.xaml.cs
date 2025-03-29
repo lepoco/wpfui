6934c55758f13f9e5173b2806fbb64820a58b13b
dosymep <biseuv@yandex.ru>
2025-03-29 16:38:21 +0600

    add default sample

---
+++ samples/Wpf.Ui.Demo.SetResources.Simple/Views/Pages/ExpanderPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using System.Windows.Controls;
+
+namespace Wpf.Ui.Demo.SetResources.Simple.Views.Pages;
+
+public partial class ExpanderPage : Page
+{
+    public ExpanderPage()
+    {
+        App.ApplyTheme(this);
+        
+        InitializeComponent();
+    }
+}
