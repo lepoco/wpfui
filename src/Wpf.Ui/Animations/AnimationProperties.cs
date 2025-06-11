a8c6e8ff14c6ee30462806e5a6a9cb1204ad0559
Biseuv Olzhas <biseuv@yandex.ru>
2025-06-11 10:55:19 +0300

    fix #1140 "System.ArgumentNullException: 'Value cannot be null. Parameter name: defaultDestinationValue'" (#1398)
    * add default sample
    
    * create issue state
    
    * add additional dependency property value
    
    * fix #1140 "System.ArgumentNullException: 'Value cannot be null. Parameter name: defaultDestinationValue'"
    
    ---------
    
    Co-authored-by: pomian <13592821+pomianowski@users.noreply.github.com>

---
+++ src/Wpf.Ui/Animations/AnimationProperties.cs
@@ -0,0 +1,27 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+namespace Wpf.Ui.Animations;
+
+internal static class AnimationProperties
+{
+    public static readonly DependencyProperty AnimationTagValueProperty = DependencyProperty.RegisterAttached(
+        "AnimationTagValue",
+        typeof(double),
+        typeof(AnimationProperties),
+        new FrameworkPropertyMetadata(
+            0.0,
+            FrameworkPropertyMetadataOptions.Inherits));
+
+    public static double GetAnimationTagValue(DependencyObject dp)
+    {
+        return (double)dp.GetValue(AnimationTagValueProperty);
+    }
+
+    public static void SetAnimationTagValue(DependencyObject dp, double value)
+    {
+        dp.SetValue(AnimationTagValueProperty, value);
+    }
+}
