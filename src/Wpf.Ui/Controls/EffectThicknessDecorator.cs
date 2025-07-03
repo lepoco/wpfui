a644fada33b7f56c71abe7c2acdaed0afece1641
Nuklon <Nuklon@users.noreply.github.com>
2025-07-03 13:41:17 +0200

    Add files via upload

---
+++ src/Wpf.Ui/Controls/EffectThicknessDecorator.cs
@@ -0,0 +1,158 @@
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace Wpf.Ui.Controls;
+
+public class EffectThicknessDecorator : Decorator
+{
+    public static readonly DependencyProperty ThicknessProperty =
+        DependencyProperty.Register(nameof(Thickness), typeof(Thickness), typeof(EffectThicknessDecorator), new PropertyMetadata(new Thickness(35), OnThicknessChanged));
+
+    private PopupContainer? _popupContainer;
+
+    public EffectThicknessDecorator()
+    {
+        SizeChanged += (_, _) => UpdateLayout();
+    }
+
+    /// <summary>
+    /// Gets or sets the thickness of the effect around the containing element.
+    /// </summary>
+    public Thickness Thickness
+    {
+        get { return (Thickness)GetValue(ThicknessProperty); }
+        set { SetValue(ThicknessProperty, value); }
+    }
+
+    /// <inheritdoc />
+    protected override int VisualChildrenCount => 1;
+
+    /// <inheritdoc />
+    protected override void OnVisualParentChanged(DependencyObject oldParent)
+    {
+        base.OnVisualParentChanged(oldParent);
+
+        if (IsInitialized)
+        {
+            SetPopupContainer();
+        }
+    }
+
+    /// <inheritdoc />
+    protected override Visual GetVisualChild(int index)
+    {
+        // Only 1 child...
+        return Child;
+    }
+
+    /// <inheritdoc />
+    protected override void OnInitialized(EventArgs e)
+    {
+        base.OnInitialized(e);
+
+        SetPopupContainer();
+    }
+
+    private void SetPopupContainer()
+    {
+        PopupContainer? popupContainer = null;
+
+        switch (VisualParent)
+        {
+            case ContextMenu contextMenu:
+                popupContainer = new PopupContainer(contextMenu);
+                break;
+            case ToolTip toolTip:
+                popupContainer = new PopupContainer(toolTip);
+                break;
+            default:
+                if (GetParentPopup(this) is { } parentPopup)
+                {
+                    popupContainer = new PopupContainer(parentPopup);
+                }
+
+                break;
+        }
+
+        if (_popupContainer?.FrameworkElement == popupContainer?.FrameworkElement)
+        {
+            return;
+        }
+
+        _popupContainer = popupContainer;
+        ApplyMargin();
+    }
+
+    private static Popup? GetParentPopup(FrameworkElement element)
+    {
+        while (true)
+        {
+            switch (element.Parent)
+            {
+                case Popup popup:
+                    return popup;
+                case FrameworkElement frameworkElement:
+                    element = frameworkElement;
+                    continue;
+            }
+
+            if (VisualTreeHelper.GetParent(element) is FrameworkElement parent)
+            {
+                element = parent;
+                continue;
+            }
+
+            return null;
+        }
+    }
+
+    private static void OnThicknessChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is EffectThicknessDecorator decorator)
+        {
+            decorator.ApplyMargin();
+        }
+    }
+
+    private void ApplyMargin()
+    {
+        _popupContainer?.SetMargin(Thickness);
+    }
+
+    private class PopupContainer
+    {
+        private readonly ContextMenu? _contextMenu;
+        private readonly Popup? _popup;
+        private readonly ToolTip? _toolTip;
+
+        public FrameworkElement? FrameworkElement => _contextMenu ?? _toolTip ?? _popup?.Child as FrameworkElement;
+
+        public PopupContainer(ContextMenu contextMenu)
+        {
+            _contextMenu = contextMenu;
+        }
+
+        public PopupContainer(ToolTip toolTip)
+        {
+            _toolTip = toolTip;
+        }
+
+        public PopupContainer(Popup popup)
+        {
+            _popup = popup;
+        }
+
+        public void SetMargin(Thickness margin)
+        {
+            if (FrameworkElement is { } frameworkElement)
+            {
+                frameworkElement.Margin = margin;
+            }
+        }
+    }
+}
