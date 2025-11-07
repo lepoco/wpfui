ae3e1ee0dea1c84de2b282cdf545a524810567da
pomian <13592821+pomianowski@users.noreply.github.com>
2025-11-07 12:41:44 +0100

    fix: Rollback XAML styling (#1563)
    * tests: Update tests
    * fix: Rollback XAML styling

--- tests/Wpf.Ui.Gallery.IntegrationTests/TitleBarTests.cs
+++ tests/Wpf.Ui.Gallery.IntegrationTests/TitleBarTests.cs
@@ -3,6 +3,9 @@
 // Copyright (C) Leszek Pomianowski and WPF UI Contributors.
 // All Rights Reserved.
 
+using System.Windows.Automation;
+using WindowVisualState = FlaUI.Core.Definitions.WindowVisualState;
+
 namespace Wpf.Ui.Gallery.IntegrationTests;
 
 public sealed class TitleBarTests : UiTest
@@ -12,13 +15,53 @@ public sealed class TitleBarTests : UiTest
     {
         Button? closeButton = FindFirst("TitleBarCloseButton").AsButton();
 
-        closeButton.Should().NotBeNull("because CloseButton should be present in the main window title bar.");
+        closeButton.Should().NotBeNull("because CloseButton should be present in the main window title bar");
         closeButton.Click(moveMouse: false);
 
-        await Wait(5);
+        await Wait(2);
 
         Application
             ?.HasExited.Should()
-            .BeTrue("because the main window should be closed after clicking the close button.");
+            .BeTrue("because the main window should be closed after clicking the close button");
+    }
+
+    [Fact]
+    public async Task MinimizeButton_ShouldHideWindow_WhenClicked()
+    {
+        Button? minimizeButton = FindFirst("TitleBarMinimizeButton").AsButton();
+
+        minimizeButton
+            .Should()
+            .NotBeNull("because MinimizeButton should be present in the main window title bar");
+        minimizeButton.Click(moveMouse: false);
+
+        await Wait(2);
+
+        MainWindow
+            .Patterns.Window.Pattern.WindowVisualState.ValueOrDefault.Should()
+            .Be(
+                WindowVisualState.Minimized,
+                "because the main window should be minimized after clicking the minimize button"
+            );
+    }
+
+    [Fact]
+    public async Task MaximizeButton_ShouldExpandWindow_WhenClicked()
+    {
+        Button? maximizeButton = FindFirst("TitleBarMaximizeButton").AsButton();
+
+        maximizeButton
+            .Should()
+            .NotBeNull("because MaximizeButton should be present in the main window title bar");
+        maximizeButton.Click(moveMouse: false);
+
+        await Wait(2);
+
+        MainWindow
+            .Patterns.Window.Pattern.WindowVisualState.ValueOrDefault.Should()
+            .Be(
+                WindowVisualState.Maximized,
+                "because the main window should be maximized after clicking the maximize button"
+            );
     }
 }
