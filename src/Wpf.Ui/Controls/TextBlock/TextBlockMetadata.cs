a910dd53573c28d3e0453b54ff98792b70d842b4
Nuklon <Nuklon@users.noreply.github.com>
2026-01-14 12:30:45 +0100

    Formatting

--- src/Wpf.Ui/Controls/TextBlock/TextBlockMetadata.cs
+++ src/Wpf.Ui/Controls/TextBlock/TextBlockMetadata.cs
@@ -9,58 +9,74 @@ internal static class TextBlockMetadata
 {
     static TextBlockMetadata()
     {
-        System.Windows.Controls.TextBlock.FontSizeProperty.OverrideMetadata(typeof(System.Windows.Controls.TextBlock),
-                                                                            new FrameworkPropertyMetadata(14d,
-                                                                                                          null,
-                                                                                                          static (d, value) =>
-                                                                                                          {
-                                                                                                              if (d.GetValue(TextBlock.FontTypographyStyleProperty) is Style style)
-                                                                                                              {
-                                                                                                                  foreach (SetterBase setterBase in style.Setters)
-                                                                                                                  {
-                                                                                                                      if (setterBase is Setter setter &&
-                                                                                                                          setter.Property == System.Windows.Controls.TextBlock.FontSizeProperty)
-                                                                                                                      {
-                                                                                                                          return setter.Value;
-                                                                                                                      }
-                                                                                                                  }
-                                                                                                              }
+        System.Windows.Controls.TextBlock.FontSizeProperty.OverrideMetadata(
+            typeof(System.Windows.Controls.TextBlock),
+            new FrameworkPropertyMetadata(
+                14d,
+                null,
+                static (d, value) =>
+                {
+                    if (d.GetValue(TextBlock.FontTypographyStyleProperty) is Style style)
+                    {
+                        foreach (SetterBase setterBase in style.Setters)
+                        {
+                            if (
+                                setterBase is Setter setter
+                                && setter.Property == System.Windows.Controls.TextBlock.FontSizeProperty
+                            )
+                            {
+                                return setter.Value;
+                            }
+                        }
+                    }
 
-                                                                                                              return value;
-                                                                                                          }));
+                    return value;
+                }
+            )
+        );
 
-        System.Windows.Controls.TextBlock.FontWeightProperty.OverrideMetadata(typeof(System.Windows.Controls.TextBlock),
-                                                                              new FrameworkPropertyMetadata(FontWeights.Regular,
-                                                                                                            null,
-                                                                                                            static (d, value) =>
-                                                                                                            {
-                                                                                                                if (d.GetValue(TextBlock.FontTypographyStyleProperty) is Style style)
-                                                                                                                {
-                                                                                                                    foreach (SetterBase setterBase in style.Setters)
-                                                                                                                    {
-                                                                                                                        if (setterBase is Setter setter &&
-                                                                                                                            setter.Property == System.Windows.Controls.TextBlock.FontWeightProperty)
-                                                                                                                        {
-                                                                                                                            return setter.Value;
-                                                                                                                        }
-                                                                                                                    }
-                                                                                                                }
+        System.Windows.Controls.TextBlock.FontWeightProperty.OverrideMetadata(
+            typeof(System.Windows.Controls.TextBlock),
+            new FrameworkPropertyMetadata(
+                FontWeights.Regular,
+                null,
+                static (d, value) =>
+                {
+                    if (d.GetValue(TextBlock.FontTypographyStyleProperty) is Style style)
+                    {
+                        foreach (SetterBase setterBase in style.Setters)
+                        {
+                            if (
+                                setterBase is Setter setter
+                                && setter.Property == System.Windows.Controls.TextBlock.FontWeightProperty
+                            )
+                            {
+                                return setter.Value;
+                            }
+                        }
+                    }
 
-                                                                                                                return value;
-                                                                                                            }));
+                    return value;
+                }
+            )
+        );
 
-        System.Windows.Controls.TextBlock.ForegroundProperty.OverrideMetadata(typeof(System.Windows.Controls.TextBlock),
-                                                                              new FrameworkPropertyMetadata(Brushes.Black,
-                                                                                                            null,
-                                                                                                            static (d, value) =>
-                                                                                                            {
-                                                                                                                if (d.GetValue(TextBlock.AppearanceForegroundProperty) is Brush brush)
-                                                                                                                {
-                                                                                                                    return brush;
-                                                                                                                }
+        System.Windows.Controls.TextBlock.ForegroundProperty.OverrideMetadata(
+            typeof(System.Windows.Controls.TextBlock),
+            new FrameworkPropertyMetadata(
+                Brushes.Black,
+                null,
+                static (d, value) =>
+                {
+                    if (d.GetValue(TextBlock.AppearanceForegroundProperty) is Brush brush)
+                    {
+                        return brush;
+                    }
 
-                                                                                                                return value is Brush ? value : Brushes.Black;
-                                                                                                            }));
+                    return value is Brush ? value : Brushes.Black;
+                }
+            )
+        );
     }
 
     public static void Initialize() { }
