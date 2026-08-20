775743505b0fc9b2d37ec3f6983c674bd323d3c3
ioswald <ianoswald@hotmail.com>
2026-08-20 11:30:54 -0500

    Add support for vector icons

---
+++ src/Wpf.Ui/Markup/VectorIconExtension.cs
@@ -0,0 +1,38 @@
+using System.Windows.Markup;
+
+using Wpf.Ui.Controls;    // <-- make sure this matches where your VectorIcon lives
+
+namespace Wpf.Ui.Markup;
+
+/// <summary>
+/// Markup extension that creates a VectorIcon from Geometry or path data.
+/// </summary>
+[ContentProperty(nameof(Data))]
+[MarkupExtensionReturnType(typeof(VectorIcon))]
+public class VectorIconExtension : MarkupExtension
+{
+    public VectorIconExtension() { }
+
+    public VectorIconExtension(Geometry data)
+    {
+        Data = data;
+    }
+
+    public VectorIconExtension(string pathData)
+    {
+        Data = Geometry.Parse(pathData);
+    }
+
+    /// <summary>
+    /// The geometry used to draw the icon.
+    /// </summary>
+    [ConstructorArgument("data")]
+    public Geometry? Data { get; set; }
+
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        return Data is null
+            ? new VectorIcon()
+            : new VectorIcon { Data = Data };
+    }
+}
